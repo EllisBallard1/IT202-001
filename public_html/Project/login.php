13fd7304676c5f974333f3f405da82fb99986520
Ellis Ballard <eab4@njit.edu>
2021-10-06 12:34:15 -0400

    Lecture updates to project files

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -32,9 +32,11 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         array_push($errors, "Email must be set");
     }
     //sanitize
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = sanitize_email($email);
     //validate
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!is_valid_email($email)){
         array_push($errors, "Invalid email address");
     }
     if (empty($password)) {
@@ -61,6 +63,8 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
                         echo "Welcome, $email";
+                        $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
                     } else {
                         echo "Invalid Password";
                     }
