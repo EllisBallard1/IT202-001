ad969918163c21468b3892acf1747bbd895559ff
Ellis Ballard <eab4@njit.edu>
2021-11-22 19:00:37 -0500

    Created Transaction Table

---
+++ public_html/Project/sql/009_create_table_transactions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS Transactions (
+    account_source INT AUTO_INCREMENT PRIMARY KEY,
+    account_dest INT,
+    balance_change INT,
+    transaction_type VARCHAR(15),
+    memo VARCHAR(20),
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expected_total INT,
+    FOREIGN KEY (account_source) REFERENCES Users(id)
+)
