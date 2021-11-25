ece740f4e72448e96237afb3ab74cac2172c5851
Ellis Ballard <eab4@njit.edu>
2021-11-25 14:54:50 -0500

    Baseline for the account creation dropdown button

--- public_html/Project/dashboard.php
+++ public_html/Project/dashboard.php
@@ -2,12 +2,11 @@
 require(__DIR__ . "/../../partials/nav.php"); ?>
 <div class="container-fluid">
     <h1>Dashboard</h1>
-    <a href='#' class="btn btn-primary">Create Account</a>
+    <a href='#' class="btn btn-secondary dropdown-toggle" name="create_account">Create Account</a>
     <a href='#' class="btn btn-primary">My Accounts</a>
     <a href='#' class="btn btn-primary">Deposit</a>
     <a href='#' class="btn btn-primary">Withdraw</a>
     <a href='#' class="btn btn-primary">Transfer</a>
     <a href="<?php echo get_url('profile.php'); ?>" class="btn btn-primary">Profile</a>
-
     
 </div>
