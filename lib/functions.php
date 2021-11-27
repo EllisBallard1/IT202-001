7385e42bf3e7413c19409ff64fcf1ecc4d93ac4c
Ellis Ballard <eab4@njit.edu>
2021-11-27 14:56:05 -0500

    Added balance page and footer to replace flash, added functions for balance and account id

--- lib/functions.php
+++ lib/functions.php
@@ -185,3 +185,18 @@ function account_creation() {
         flash("You're not logged in", "danger");
     }
 }
+
+
+function get_account_balance() {
+    if (is_logged_in() && isset($_SESSION["user"]["account"])) {
+        return (int)se($_SESSION["user"]["account"], "balance", 0, false);
+    }
+    return 0;
+}
+
+function get_user_account_id() {
+    if (is_logged_in() && isset($_SESSION["user"]["account"])) {
+        return (int)se($_SESSION["user"]["account"], "id", 0, false);
+    }
+    return 0;
+}
---
+++ partials/balance.php
@@ -0,0 +1,12 @@
+<div id="balance-value">
+    Balance: <?php echo get_account_balance(); ?>
+</div>
+<script>
+    let bv = document.getElementById("balance-value");
+    let placeholders = document.getElementsByClassName("show-balance");
+    for (let p of placeholders) {
+        p.innerHTML = bv.outerHTML;
+    }
+    bv.remove();
+    
+</script>
