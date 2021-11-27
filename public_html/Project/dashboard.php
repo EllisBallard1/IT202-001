f32a6e92f5a7a68fb7387dfee8575f681c325188
Ellis Ballard <eab4@njit.edu>
2021-11-26 22:49:44 -0500

    Successful dropdown button for account creation options

--- public_html/Project/dashboard.php
+++ public_html/Project/dashboard.php
@@ -2,11 +2,19 @@
 require(__DIR__ . "/../../partials/nav.php"); ?>
 <div class="container-fluid">
     <h1>Dashboard</h1>
-    <a href='#' class="btn btn-secondary dropdown-toggle" name="create_account">Create Account</a>
-    <a href='#' class="btn btn-primary">My Accounts</a>
-    <a href='#' class="btn btn-primary">Deposit</a>
-    <a href='#' class="btn btn-primary">Withdraw</a>
-    <a href='#' class="btn btn-primary">Transfer</a>
-    <a href="<?php echo get_url('profile.php'); ?>" class="btn btn-primary">Profile</a>
-    
+    <h2>Might need to use an id tag isntead of a name tag</h2>
+    <div class="dropdown mt-3">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="book-dropdown" data-bs-toggle="dropdown">
+            Create Account
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="book-dropdown">
+            <li><a href="#" class="dropdown-item">Checking Account</a>
+            <li><a href="#" class="dropdown-item">Savings Account</a> 
+        </ul>
+        <a href='#' class="btn btn-primary">My Accounts</a>
+        <a href='#' class="btn btn-primary">Deposit</a>
+        <a href='#' class="btn btn-primary">Withdraw</a>
+        <a href='#' class="btn btn-primary">Transfer</a>
+        <a href="<?php echo get_url('profile.php'); ?>" class="btn btn-primary">Profile</a>
+    </div>
 </div>
