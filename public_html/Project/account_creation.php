e540912f1ce0515cd0e15c67a1562121d7b5010d
Ellis Ballard <eab4@njit.edu>
2021-11-29 15:22:09 -0500

    Button dropdown template for account creation page

--- public_html/Project/account_creation.php
+++ public_html/Project/account_creation.php
@@ -2,3 +2,15 @@
 require(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Account Creation</h1>
+<div class="container fluid">
+    <div class="dropdown mt-3">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+            Account Type
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="book-dropdown">
+            <li><a href="#" class="dropdown-item">Checking</a></li>
+            <li><a href="#" class="dropdown-item">Savings</a></li>
+        </ul>
+    </div>
+</div>
+
