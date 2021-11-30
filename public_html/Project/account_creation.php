49e21eda1c9f2dac20eebbe98ce69f4d2f758dcb
EllisBallard <ellisballard16@gmail.com>
2021-11-30 00:07:54 -0500

    Account creation page template with bootstrap implemented and form created ready to event handler

--- public_html/Project/account_creation.php
+++ public_html/Project/account_creation.php
@@ -1,16 +1,31 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-<h1>Account Creation</h1>
-<div class="container fluid">
-    <div class="dropdown mt-3">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-            Account Type
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="book-dropdown">
-            <li><a href="#" class="dropdown-item">Checking</a></li>
-            <li><a href="#" class="dropdown-item">Savings</a></li>
-        </ul>
-    </div>
+<div class="container-fluid">
+    <h1>Account Creation</h1>
+    <form  onsubmit="return validate(this)" method="POST">
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="account_type" id="checking_account">
+            <label class="form-check-label" for="checking_account">
+                Checking Account
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="account_type" id="savings_account">
+            <label class="form-check-label" for="savings_account">
+                Savings Account
+            </label>
+        </div>
+        <div class="input-group mb-3">
+            <span class="input-group-text">$</span>
+                <input type="text" placeholder="Please enter a minimum of $5 for your first deposit" class="form-control" aria-label="Amount (to the nearest dollar)">
+            <span class="input-group-text">.00</span>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Create Account"/>
+    </form>
 </div>
+
+
+
+
 
