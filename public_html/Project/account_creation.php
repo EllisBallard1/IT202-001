bd3850cc14928388321ea70f5bea774464475dda
Ellis Ballard <eab4@njit.edu>
2021-12-02 18:31:08 -0500

    beginning of php for account_creation page

--- public_html/Project/account_creation.php
+++ public_html/Project/account_creation.php
@@ -2,8 +2,8 @@
 require(__DIR__ . "/../../partials/nav.php");
 is_logged_in(true);
 ?>
+<h1 class="text-center mt-4">Create New Account</h1>
 <div class="container-fluid">
-    <h1>Account Creation</h1>
     <form  onsubmit="return validate(this)" method="POST">
         <div class="form-check">
             <input class="form-check-input" type="radio" name="account_type" id="checking_account">
@@ -22,10 +22,19 @@ is_logged_in(true);
                 <input type="number" id="initial_deposit" name = "deposit" placeholder="Please enter a minimum of $5 for your first deposit" class="form-control" min="5.0" step="0.01">
             <span class="input-group-text">.00</span>
         </div>
-        <input type="submit" class="btn btn-primary" value="Create Account"/>
+        <input type="submit" name="create" class="btn btn-primary" value="Create Account"/>
     </form>
 </div>
 
+<?php
+if (isset($_POST["create"])) {
+    $db = getDB();
+    $query = $db->prepare("SELECT account_num FROM Accounts WHERE account_num = :acc_num");
+    //now need to call the function to create the account
+    //but how do I call the function properly
+}
+
+?>
 
 
 
